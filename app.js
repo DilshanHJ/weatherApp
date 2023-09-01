7b8a7587dc170d2b6a597459cc90389a50641654
Dilshan Hashitha <hashithadiljay@gmail.com>
2023-09-01 18:51:24 +0530

    4th commit

--- app.js
+++ app.js
@@ -233,7 +233,7 @@ app.get('/weatherofdate', async (req, res) => {
 
 
 
-app.listen(process. env. PORT || 3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
       console.log("Server is listening at port 3000...")
 });
 
